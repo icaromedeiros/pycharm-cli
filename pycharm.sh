f2e80750bfcce030a69a4601a1f2f979d3685ef6
Icaro Medeiros <icaro.medeiros@gmail.com>
2014-06-03 14:34:25 -0300

    a simple shell script, still quite dumb

---
+++ pycharm.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Example in MacOS
+
+# Uncomment the line below to include PYCHARM_BIN variable
+PYCHARM_BIN="/Applications/PyCharm.app/Contents/MacOS/pycharm"
+
+# Comment this line below if you have PYCHARM_BIN defined
+: ${PYCHARM_BIN?"Need to set PYCHARM_BIN"}
+
+pwd=`pwd`
+if [ "$1" != "" ]; then
+    target="$pwd/$1"
+    echo "Opening PyCharm in directory $target"
+    $PYCHARM_BIN $target
+else
+    echo "Positional parameter 1 is empty, default is to open PyCharm using current directory"
+    $PYCHARM_BIN $pwd
+fi
